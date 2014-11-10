e483da5ff4adbb8650aef7a3bd583a9000ac3e97
David Souther <davidsouther@gmail.com>
2014-11-10 09:04:18 -0500

    Changed test to use JS comments in bock regex.

--- test/blockregex.js
+++ test/blockregex.js
@@ -1,14 +1,13 @@
 var source =
     "OPERATOR = /// ^ (\n" +
-    "  ?: [-=]>             # function\n" +
-    "   | [-+*/%<>&|^!?=]=  # compound assign / compare\n" +
-    "   | >>>=?             # zero-fill right shift\n" +
-    "   | ([-+:])\1         # doubles\n" +
-    "   | ([&|<>])\2=?      # logic / shift\n" +
-    "   | \?\.              # soak access\n" +
-    "   | \.{2,3}           # range or splat\n" +
+    "  ?: [-=]>             // function\n" +
+    "   | [-+*/%<>&|^!?=]=  // compound assign / compare\n" +
+    "   | >>>=?             // zero-fill right shift\n" +
+    "   | ([-+:])\1         // doubles\n" +
+    "   | ([&|<>])\2=?      // logic / shift\n" +
+    "   | \?\.              // soak access\n" +
+    "   | \.{2,3}           // range or splat\n" +
     ") ///\n" ;
-
 
 var expected = "OPERATOR = /^(?:[-=]>|[-+*\/%<>&|^!?=]=|>>>=?|([-+:])\1|([&|<>])\2=?|\?\.|\.{2,3})/;\n";
 
