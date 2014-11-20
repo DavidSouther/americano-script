4bc0dfa68986e3c6ab5f20bd3aada2bac40c67cf
David Souther <davidsouther@gmail.com>
2014-11-20 16:55:06 -0500

    Non-experimental splices work.

--- test/splice.js
+++ test/splice.js
@@ -7,9 +7,6 @@ describe('Splices', function(){
         "foo.bar[ i .. j ]": "foo.bar.slice(i, +j + 1 || 9e9)\n",
         "foo.bar[m...n] = []": "foo.bar.splice(m, n - m, [])\n",
         "foo.bar[m ... n][i..j] = []": "foo.bar.slice(m, n).splice(i, j - i + 1, [])\n",
-        "[a...b]": "(function() { var _ = []; for (var i = a; a <= b ? i < b : i > b; a <= b ? i+=1 : i-=1){ _.push(i); } return _; }())\n",
-        "[a..b]":  "(function() { var _ = []; for (var i = a; a <= b ? i <= b : i >= b; a <= b ? i+=1 : i-=1){ _.push(i); } return _; }())\n",
-        "[a..b, -2]": "(function() { var _ = []; for (var i = a; -2 > 0 ? i <= b : i >= b; a <= b ? i+=2 : i-=2){ _.push(i); } return _; }())\n",
     };
 
     it('slices and splices and dices', function(){
