719caa3a372886035a4567dde593ae1e7bc5d05f
David Souther <davidsouther@gmail.com>
2014-11-20 13:10:17 -0500

    Add literal single quote and variable expansion.

--- test/blockregex.js
+++ test/blockregex.js
@@ -7,9 +7,11 @@ var source =
     "   | ([&|<>])\2=?      // logic / shift\n" +
     "   | \?\.              // soak access\n" +
     "   | \.{2,3}           // range or splat\n" +
+    "   | #{foo}            // Variable Interpolation" +
+    "   | 'literal'         // literal with quotes" +
     ") ///\n" ;
 
-var expected = "OPERATOR = /^(?:[-=]>|[-+*\/%<>&|^!?=]=|>>>=?|([-+:])\1|([&|<>])\2=?|\?\.|\.{2,3})/;\n";
+var expected = "OPERATOR = new RegExp('^(?:[-=]>|[-+*\/%<>&|^!?=]=|>>>=?|([-+:])\1|([&|<>])\2=?|\?\.|\.{2,3}|' + foo + '|\\'literal\\')');\n";
 
 var compiler = require('../lib/americano');
 
